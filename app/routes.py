3a10a26b17fc8833dcb0abc70104bfb8288fc09f
AryanB-23bit <bishta1@southernct.edu>
2021-04-02 20:53:36 -0400

    Started working on DB joins. Its not done yet but got some tables to join

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import User
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
 from app.forms import CreateUser, LoginUser
 from werkzeug.security import generate_password_hash, check_password_hash
